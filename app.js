0cb909f6efc988441dabfe16d1606c4482fca043
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-15 23:31:11 +0500

    Initial commit

--- app.js
+++ app.js
@@ -12,3 +12,39 @@ var fullName = userFirstName + " " + userLastName;
 
 alert("Hello " + fullName + "!");
 console.log("Hello " + fullName + "!");
+
+// Answer2
+
+var model = prompt("Enter your favorite mobile phone model:");
+
+var lengthOfInput = model.length;
+
+alert("Length of your input is: " + lengthOfInput);
+console.log("Length of your input is: " + lengthOfInput);
+
+// Answer3
+
+var word = "Pakistani";
+
+var index = word.indexOf("n");
+
+alert("Index of 'n' in 'Pakistani' is: " + index);
+console.log("Index of 'n' in 'Pakistani' is: " + index);
+
+// Answer4
+
+var text = "Hello World";
+
+var lastIndex = text.lastIndexOf("l");
+
+alert("Last index of 'l' in 'Hello World' is: " + lastIndex);
+console.log("Last index of 'l' in 'Hello World' is: " + lastIndex);
+
+// Answer5
+
+var word = "Pakistani";
+
+var result = word.charAt(3);
+
+alert("Character at index 3 is: " + result);
+console.log("Character at index 3 is: " + result);
